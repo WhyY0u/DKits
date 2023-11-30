2a1794e24aec08c3cd17c16493fc8d539e0087e3
aimsiBTW <80332836+aimsiBTW@users.noreply.github.com>
2023-11-30 19:59:26 +0600

    renderParticle

---
+++ Loading.cpp
@@ -0,0 +1,19 @@
+#include "Loading.h"
+void Loading::render(FT_Library ft) {
+}
+
+void Loading::init()
+{
+}
+
+void Loading::close()
+{
+}
+
+void Loading::mouseClicked(int button, int action)
+{
+}
+
+void Loading::keyTyped()
+{
+}
--- Loading.h
+++ Loading.h
@@ -1,7 +1,13 @@
 #include "Scene.h"
-class Loading {
+class Loading : public Scene {
 public:
+	Loading(std::string name) : Scene(name) {};
+	void render(FT_Library ft) override;
+	void init() override;
+	void close() override;
+	void mouseClicked(int button, int action) override;
+	void keyTyped() override;
+	void renderParticle() {
 
-private:
-
+	}
 };
